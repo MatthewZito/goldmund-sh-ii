e4f7bf658fca597dad0e05ad373989d04fdfb388
goldmund <matthewtzito@gmail.com>
2021-08-18 09:01:22 -0700

    chore: add tape for unit testing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # goldmund.sh ii
+
+## Services
+
+### Frontend
+
+### API + GraphQL
+
+### Database
+
+### Command-line Interface
--- src/state/modules/blog/state.js
+++ src/state/modules/blog/state.js
@@ -1,3 +1,3 @@
 export default () => ({
-  posts: JSON.parse(sessionStorage.getItem(import.meta.env.VITE_POSTS_STORAGE_KEY)) || []
+  posts: JSON.parse(sessionStorage.getItem(import.meta.env.VITE_APP_POSTS_STORAGE_KEY)) || []
 });
